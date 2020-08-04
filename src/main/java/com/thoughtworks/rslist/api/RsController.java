afdbab7f99c4fe5bb0eaa67b262fc84773b40cd6
zkj <438101845@qq.com>
2020-08-04 23:21:44 +0800

    [zkj]-feat:(data-binding) finish get event by index api

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -8,4 +10,11 @@ import java.util.List;
 @RestController
 public class RsController {
   private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+
+  @GetMapping("/rs/list/{index}")
+  public String getRsEventStringByIndex(@PathVariable int index) {
+    return rsList.get(index - 1);
+  }
 }
+
+
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,10 +1,28 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class RsListApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_event_given_index() throws Exception {
+        mockMvc.perform(get("/rs/list/1"))
+                .andExpect(content().string("第一条事件"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void contextLoads() {
