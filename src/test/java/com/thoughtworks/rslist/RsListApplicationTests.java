54771674639a81ef0f3a19ebed1a5da38c2444cb
zkj <438101845@qq.com>
2020-08-04 23:04:55 +0800

    [zkj]-feat:routing-test get rsEvent list

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,10 +1,28 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class RsListApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_return_list_given_api_of_rs_list() throws Exception {
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void contextLoads() {
