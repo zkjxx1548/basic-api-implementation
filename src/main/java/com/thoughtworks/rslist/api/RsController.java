81edf296d347bb9a7003b89753e1a98d0cac1114
zkj <438101845@qq.com>
2020-08-04 23:45:39 +0800

    [zkj]-feat:(data-binding) finish get event by start to end api

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -14,6 +15,15 @@ public class RsController {
   @GetMapping("/rs/list/{index}")
   public String getRsEventStringByIndex(@PathVariable int index) {
     return rsList.get(index - 1);
+  }
+
+  @GetMapping("/rs/list")
+  public String getRsEventStringByStartToEnd(@RequestParam Integer start, @RequestParam Integer end) {
+    if (start != null && end != null) {
+      return rsList.subList(start - 1, end).toString();
+    }
+    return rsList.toString();
+
   }
 }
 
