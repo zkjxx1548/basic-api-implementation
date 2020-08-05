0341d36211c3054a5bf9d807d843ae07868336b7
zkj <438101845@qq.com>
2020-08-05 10:21:34 +0800

    [zkj]-feat: add User.class

---
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -0,0 +1,70 @@
+package com.thoughtworks.rslist.domain;
+
+public class User {
+    private String userName;
+    private String gender;
+    private int age;
+    private String email;
+    private String phone;
+    private int voteNum = 10;
+
+    public User() {
+    }
+
+    public User(String userName, String gender, int age, String email, String phone, int voteNum) {
+        this.userName = userName;
+        this.gender = gender;
+        this.age = age;
+        this.email = email;
+        this.phone = phone;
+        this.voteNum = voteNum;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getVoteNum() {
+        return voteNum;
+    }
+
+    public void setVoteNum(int voteNum) {
+        this.voteNum = voteNum;
+    }
+}
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -2,10 +2,7 @@ package com.thoughtworks.rslist;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thoughtworks.rslist.domain.RsEvent;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
