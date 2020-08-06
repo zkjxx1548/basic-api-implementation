6680b3fc44a71fa0ffbf89b6711faa84e6fee345
zkj <438101845@qq.com>
2020-08-06 08:42:03 +0800

    [zkj]-feat: add logger

--- src/main/java/com/thoughtworks/rslist/componet/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/componet/GlobalExceptionHandler.java
@@ -1,38 +1,36 @@
 package com.thoughtworks.rslist.componet;
 
-import com.thoughtworks.rslist.domain.RsEvent;
 import com.thoughtworks.rslist.exception.Error;
 import com.thoughtworks.rslist.exception.RsEventNotValidException;
 import com.thoughtworks.rslist.exception.StartOrEndNotValidException;
+import com.thoughtworks.rslist.exception.UserNotValidException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.validation.Valid;
-
 @ControllerAdvice
-public class RsEventExceptionHandler {
+public class GlobalExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler({RsEventNotValidException.class, MethodArgumentNotValidException.class})
+    @ExceptionHandler({RsEventNotValidException.class, MethodArgumentNotValidException.class, StartOrEndNotValidException.class, UserNotValidException.class})
     public ResponseEntity<Error> rsEven(Exception e) {
+
         String errorMessage;
         if (e instanceof RsEventNotValidException) {
             errorMessage = "invalid index";
-        } else {
+        } else if (e instanceof MethodArgumentNotValidException){
             errorMessage = "invalid param";
+        } else if (e instanceof StartOrEndNotValidException) {
+            errorMessage = "invalid request param";
+        } else {
+            errorMessage = "invalid user";
         }
         Error error = new Error();
         error.setError(errorMessage);
-
-        return ResponseEntity.badRequest().body(error);
-    }
-
-    @ExceptionHandler(StartOrEndNotValidException.class)
-    public ResponseEntity<Error> inValidIndexOfStartOrEnd(Exception e) {
-        String errorMessage = "invalid request param";
-        Error error = new Error();
-        error.setError(errorMessage);
+        LOGGER.error(error.getError());
 
         return ResponseEntity.badRequest().body(error);
     }
